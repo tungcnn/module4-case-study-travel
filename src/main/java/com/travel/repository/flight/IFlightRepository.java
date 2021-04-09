e060cdf1db41ad24b55ebc139230f9693eb22079
tungcnn <sontung.cnn@gmail.com>
2021-04-09 17:22:45 +0700

    delete querry

--- src/main/java/com/travel/repository/flight/IFlightRepository.java
+++ src/main/java/com/travel/repository/flight/IFlightRepository.java
@@ -1,10 +1,7 @@
 package com.travel.repository.flight;
 
 import com.travel.model.flight.Flight;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface IFlightRepository extends PagingAndSortingRepository<Flight, Long> {
-    @Query("")
-    Iterable<Flight> search(String string);
 }
