5a19b87ad66cd29f73a5fe479f7320c272a3f39a
dqa97 <72239024+dqa97@users.noreply.github.com>
2021-04-12 15:32:08 +0700

    add RegistrationController

--- src/main/java/com/travel/service/role/RoleService.java
+++ src/main/java/com/travel/service/role/RoleService.java
@@ -3,7 +3,9 @@ package com.travel.service.role;
 import com.travel.model.AppRole;
 import com.travel.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RoleService implements IRoleService {
     @Autowired
     private RoleRepository roleRepository;
