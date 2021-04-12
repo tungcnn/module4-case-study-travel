e39d952b0eb79d2cac63c1378fe76cf81553a112
tungcnn <sontung.cnn@gmail.com>
2021-04-12 14:24:51 +0700

    flight pagination

--- src/main/java/com/travel/repository/flight/IFlightRepository.java
+++ src/main/java/com/travel/repository/flight/IFlightRepository.java
@@ -1,6 +1,7 @@
 package com.travel.repository.flight;
 
 import com.travel.model.flight.Flight;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface IFlightRepository extends PagingAndSortingRepository<Flight, Long> {
