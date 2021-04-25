8bc45d3cd9afbd39a4e7c11b320369dd2051861d
tungcnn <sontung.cnn@gmail.com>
2021-04-25 21:59:01 +0700

    fix tour search bõ

--- src/main/java/com/travel/controller/HomeController.java
+++ src/main/java/com/travel/controller/HomeController.java
@@ -11,12 +11,14 @@ import com.travel.service.tour.ITourService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class HomeController {
     @Autowired
     private IFlightLocationService flightLocationService;
